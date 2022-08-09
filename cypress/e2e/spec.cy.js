6e9ce2161f7005898b07636cb5e51fca197e8bda
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-09 13:09:05 -0400

    Add test user input for Form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,29 +8,30 @@ describe('URL Shortener', () => {
     .visit("http://localhost:3000/")
   });
 
-  it('should be able to visit the page and see title and exisiting shortened URLs', () => {
+  it("When a user visits the page, they can view the page title and the existing shortened URLs", () => {
     cy.url().should("eq", "http://localhost:3000/")
     .get(".app-title").should("be.visible").contains("URL Shortener")
     .get(".url-container").find(".url-card").contains("h3", "Awesome photo")
     .get("a").contains("http://localhost:3001/useshorturl/1")
   });
 
-  it("should be able to view the Form with the proper inputs", () => {
+  it("When a user visits the page, they can view the Form with the proper inputs", () => {
     cy.get(".form-container")
     .get("input[name='title']")
     .get("input[name='urlToShorten']")
     .get("button").first().contains("Shorten Please!")
   });
 
+  it("When a user fills out the form, the information is reflected in the input fields", () => {
+    cy.get(".form-container")
+    .get("input[name='title']").type("Travis rocks!").should("have.value", "Travis rocks!")
+    .get("input[name='urlToShorten']").type("anExtraLongURLShouldGoHere").should("have.value", "anExtraLongURLShouldGoHere")
+    .get("button").first().contains("Shorten Please!")
+  });
+
 });
 
 
-
-
-// Iteration 3
-
-// When a user visits the page, they can view the Form with the proper inputs
-// When a user fills out the form, the information is reflected in the input fields
 
 // Iteration 4
 // Write Cypress tests for the following user flows (don't forget to stub your network requests):

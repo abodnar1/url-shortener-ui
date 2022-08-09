1381b42c14874d30c00de6397d653106a6643a35
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-09 11:57:57 -0400

    Add useEffect for GET request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
 const App = () => {
-  const [urls, setUrls] = useState([
-    {
-      id: 1,
-      long_url: "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
-      short_url: "http://localhost:3001/useshorturl/1",
-      title: "Awesome photo"
-    }
-  ]);
+  const [urls, setUrls] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getUrls()
+    .then(data => setUrls(data.urls))
+    .catch(err => setError(err.message))
+  }, []);
 
   return (
     <main className="App">
@@ -20,7 +20,7 @@ const App = () => {
         <h1>URL Shortener</h1>
         <UrlForm />
       </header>
-
+      {error && <p className="error-message">{error}</p>}
       <UrlContainer urls={urls} />
     </main>
   );
@@ -43,3 +43,13 @@ export default App;
   //   }
 
   // render() {
+
+
+// [
+//   {
+//     id: 1,
+//     long_url: "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+//     short_url: "http://localhost:3001/useshorturl/1",
+//     title: "Awesome photo"
+//   }
+// ]

57153128400e393c66312ebffb6fef6d26750138
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-09 12:11:23 -0400

    Refactor form to use hooks; fix bugs

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -1,53 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class UrlForm extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-    this.state = {
-      title: '',
-      urlToShorten: ''
-    };
-  }
+const UrlForm = () => {
+  const [title, setTitle] = useState("");
+  const [urlToShorten, setUrlToShorten] = useState("");
 
-  handleNameChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.clearInputs();
-  }
+    // need to make a new object
+    // will need to call addUrlToShorten() here
+    console.log("Hi")
+    clearInputs();
+  };
 
-  clearInputs = () => {
-    this.setState({title: '', urlToShorten: ''});
-  }
+   const clearInputs = () => {
+    setTitle("");
+    setUrlToShorten("");
+  };
+  
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Title...'
+        name='title'
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
 
-  render() {
-    return (
-      <form>
-        <input
-          type='text'
-          placeholder='Title...'
-          name='title'
-          value={this.state.title}
-          onChange={e => this.handleNameChange(e)}
-        />
+      <input
+        type='text'
+        placeholder='URL to Shorten...'
+        name='urlToShorten'
+        value={urlToShorten}
+        onChange={e => setUrlToShorten(e.target.value)}
+      />
 
-        <input
-          type='text'
-          placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
-          onChange={e => this.handleNameChange(e)}
-        />
+      <button onClick={e => handleSubmit(e)}>
+        Shorten Please!
+      </button>
+    </form>
+  );
+};
 
-        <button onClick={e => this.handleSubmit(e)}>
-          Shorten Please!
-        </button>
-      </form>
-    )
-  }
-}
+// class UrlForm extends Component {
+//   constructor(props) {
+//     super();
+//     this.props = props;
+//     this.state = {
+//       title: '',
+//       urlToShorten: ''
+//     };
+//   }
+
+  // handleNameChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // }
+
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   this.clearInputs();
+  // }
+
+  // clearInputs = () => {
+  //   this.setState({title: '', urlToShorten: ''});
+  // }
+
+  // render() {
+  //   return (
+  //     <form>
+  //       <input
+  //         type='text'
+  //         placeholder='Title...'
+  //         name='title'
+  //         value={this.state.title}
+  //         onChange={e => this.handleNameChange(e)}
+  //       />
+
+  //       <input
+  //         type='text'
+  //         placeholder='URL to Shorten...'
+  //         name='title'
+  //         value={this.state.title}
+  //         onChange={e => this.handleNameChange(e)}
+  //       />
+
+  //       <button onClick={e => this.handleSubmit(e)}>
+  //         Shorten Please!
+  //       </button>
+  //     </form>
+  //   )
+  // }
+// }
 
 export default UrlForm;
